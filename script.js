f0403ec2b052cc41cb631140fb5cb9b422fe3043
ShajithKAJ <shajith.it.is@gmail.com>
2022-03-12 06:06:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+const image = document.querySelectorAll('img');
+const score = document.querySelector('.score');
+const timer = document.querySelector('.timer');
+const goal = document.querySelector('.goal');
+
+goal.innerHTML = prompt('Set Goal','60');
+
+const interval = setInterval(() => {
+    timer.innerHTML--;
+
+    if(timer.innerHTML == 0 || timer.innerHTML < 0) {
+        alert('Time Up');
+        if(goal.innerHTML > score.innerHTML) {
+            alert('You Lose!!!');
+        }
+        else if(goal.innerHTML < score.innerHTML) {
+            alert('You Won!!!');
+        }
+        //clearInterval(interval);
+        score.innerHTML = 0;
+        timer.innerHTML = 60;
+    }
+}, 1000);
+
+image.forEach(element=>{
+    element.addEventListener('click',countIncrease);
+});
+
+function countIncrease() {
+    count++;
+    score.innerHTML = count;
+}
